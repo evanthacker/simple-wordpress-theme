aa63a808f67db9c04bb7700d99e647b0680961a8
evanthacker <thackerevan@gmail.com>
2024-10-24 16:59:15 -0400

    basic theme setup

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<header>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <title><?php bloginfo('name'); ?></title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+    <?php wp_head(); ?>
+</header>
+<body>
+    <header>
+        <h1><?php bloginfo('name'); ?></h1>
+        <small><?php bloginfo('description'); ?></small>
+    </header>
+</body>
+
+</html>
