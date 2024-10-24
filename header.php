8dbc27fbd36ba6446605497590c1a6a48bf8ef32
evanthacker <thackerevan@gmail.com>
2024-10-24 18:16:27 -0400

    footer & header php files

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<header>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <title><?php bloginfo('name'); ?></title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+    <?php wp_head(); ?>
+</header>
+<body>
+    <header>
+    <div class="container">
+        <h1><?php bloginfo('name'); ?></h1>
+        <span><?php bloginfo('description'); ?></span>
+    </div>
+    </header>
