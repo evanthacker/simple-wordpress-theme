a57295c68743189c708d40206deec32c4615e4cd
evanthacker <thackerevan@gmail.com>
2024-10-31 12:05:05 -0400

    created single.php and Css

---
+++ single.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+    <div class="main">
+    <div class="container">
+
+    <?php if(have_posts()) : ?>
+        <?php while(have_posts()): the_post(); ?>
+            <article class="post">
+                <h3>
+                    <?php the_title(); ?>
+                </h3>
+                    <div class="meta">
+                        Created By: <?php the_author(); ?> on <?php the_date("l jS \of F Y h:i:s A"); ?>
+                    </div>
+                <?php the_content(); ?>
+            </article>
+        <?php endwhile; ?>
+    <?php else : ?>
+        <?php echo wpautop('Sorry, No posts were found.'); ?>
+
+    <?php endif; ?>
+    </div>
+    </div>
+
+<?php get_footer(); ?>
