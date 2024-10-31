5dc9effb877822ab01d7459d26f6dd8969bff537
evanthacker <thackerevan@gmail.com>
2024-10-31 12:35:06 -0400

    added functions.php with postthumbnail working, added to index.php postthumbnail, and added CSS to go along with it.

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
     <?php if(have_posts()) : ?>
         <?php while(have_posts()): the_post(); ?>
             <article class="post">
+            <?php if(has_post_thumbnail()) : ?>
+                    <div class="post-thumbnail">
+                        <?php the_post_thumbnail(); ?>
+                    </div>
                 <h3>
                     <a href="<?php the_permalink(); ?>">
                         <?php the_title(); ?>
@@ -13,6 +17,8 @@
                     <div class="meta">
                         Created By: <?php the_author(); ?> on <?php the_date("l jS \of F Y h:i:s A"); ?>
                     </div>
+                    
+                <?php endif; ?>
                 <?php the_content(); ?>
                 <br>
                 <a class="button" href="<?php the_permalink(); ?>">
--- single.php
+++ single.php
@@ -5,6 +5,7 @@
     <?php if(have_posts()) : ?>
         <?php while(have_posts()): the_post(); ?>
             <article class="post">
+                
                 <h3>
                     <?php the_title(); ?>
                 </h3>
