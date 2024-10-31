be9e37e9b12eec46cd4b6cf7337a2ffcb538bc9a
evanthacker <thackerevan@gmail.com>
2024-10-31 15:33:59 -0400

    page.php

---
+++ page.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+    <div class="main">
+    <div class="container">
+
+
+            <article class="post">
+
+                <h3>
+                    <?php the_title(); ?>
+                </h3>
+                    
+                <?php the_content(); ?>
+         
+            </article>
+
+
+    </div>
+    </div>
+
+<?php get_footer(); ?>
